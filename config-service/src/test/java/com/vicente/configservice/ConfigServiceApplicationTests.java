def7418c17ddd1c7c9c448f53e360900be66631f
vicentemarti31416 <vicentemarti31416@gmail.com>
2022-08-08 22:02:01 +0200

    config server

---
+++ config-service/src/test/java/com/vicente/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicente.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
